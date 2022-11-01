9e84d2ce9f8a432f49b77736a190a8fa3671a11d
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-11-01 11:11:10 -0300

    code
    code

--- Js e Html/js/script.js
+++ Js e Html/js/script.js
@@ -1,1 +1,16 @@
-alert("Isso sim é um programa.");
+//alert("Isso é um programa?");
+
+document.write("A idade do André é: 18");
+document.write("</br>");
+
+var ano = 2016;
+document.write("Flávio tem " + (ano - 1977) + " anos.");
+document.write("</br>");
+
+var media = (39 + 20 + 41) / 3;
+document.write("A media é: " + media);
+document.write("</br>");
+
+
+
+
